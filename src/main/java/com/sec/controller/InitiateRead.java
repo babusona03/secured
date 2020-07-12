9e7a5810de1440ce78e7410db421a261ade4d4ef
babusona03 <babusona03@gmail.com>
2020-07-12 23:52:08 +0530

    embedded identity store definition problem resolved.

--- src/main/java/com/sec/controller/InitiateRead.java
+++ src/main/java/com/sec/controller/InitiateRead.java
@@ -22,20 +22,34 @@ import org.glassfish.soteria.identitystores.annotation.EmbeddedIdentityStoreDefi
 @ServletSecurity(@HttpConstraint(rolesAllowed = {"user","admin"}))
 @BasicAuthenticationMechanismDefinition(realmName="security-realm")
 @EmbeddedIdentityStoreDefinition({
-	@Credentials(callerName = "user",  password = "password", 	groups = {"user" }),
-	@Credentials(callerName = "admin", password = "admin", 		groups = {"admin"}),
-	@Credentials(callerName = "guest", password = "guest", 		groups = {"guest"})
+	@Credentials(callerName = "user",  password = "password123", 	groups = {"user" }),
+	@Credentials(callerName = "admin", password = "admin123", 		groups = {"admin"}),
+	@Credentials(callerName = "guest", password = "guest123", 		groups = {"guest"})	
 	})
+
+
+/*
+ * Notes:
+ * only this servlet is required to test run embeddedidentitystoredefinition
+ * with JASPIC enabled by placing jboss-web.xml,any HAM implementation causes ambiguous dependency by weld,
+ * soteria's implementation collides with that of the custom one.
+ * this can be bypassed by implementing IdentityStore and overriding the validate() for credential validation purpose.
+ * now, without jboss-web.xml, JASPIC won't kick in.so,jboss-web.xml is a must to implement security with wildfly.
+ * in this implementation, the validation mechanism is delegated to soteria's own HAM implementation.
+ * works fine. 
+ *
+ * */
 public class InitiateRead extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
+  
 
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	  RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/jsf/read.xhtml");
+//	  RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/jsf/read.xhtml");
 	  System.out.println("inside read initiator servlet.");
-	  //view.forward(request, response);
+//	  view.forward(request, response);
 
 
   }
@@ -46,7 +60,9 @@ public class InitiateRead extends HttpServlet {
   
   /*
    * context: calling a servlet from jsf command button onclick
-   What you are trying to attempt will not work. The moment you start using JSF tags / components, any reference to a bean or object, JSF tries to look within its context, facesconfig. It does not go outside its perimeter.
-If your intention is to all call a servlet by clicking a link within a page rendered by JSF, use regualr HTML tags or call a managed bean and then forward to a servlet
+   What you are trying to attempt will not work. The moment you start using JSF tags / components, any reference to a bean or object,
+    JSF tries to look within its context, facesconfig. It does not go outside its perimeter.
+If your intention is to all call a servlet by clicking a link within a page rendered by JSF, use regualr HTML tags or call 
+a managed bean and then forward to a servlet
 */
 }
--- src/main/java/com/sec/service/ContentAuthenticationMechanism.java
+++ src/main/java/com/sec/service/ContentAuthenticationMechanism.java
@@ -1,97 +1,97 @@
-package com.sec.service;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.security.enterprise.AuthenticationException;
-import javax.security.enterprise.AuthenticationStatus;
-import javax.security.enterprise.authentication.mechanism.http.AutoApplySession;
-import javax.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
-
-
-import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
-import javax.security.enterprise.credential.UsernamePasswordCredential;
-import javax.security.enterprise.identitystore.CredentialValidationResult;
-import javax.security.enterprise.identitystore.CredentialValidationResult.Status;
-import javax.security.enterprise.identitystore.IdentityStoreHandler;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-@ApplicationScoped
-//In order to ensure that it is available to the Java application, 
-//you will need to implement the HttpAuthenticationMechanism interface as a CDI bean with @ApplicationScope.
-@AutoApplySession
-
-//https://developer.ibm.com/tutorials/j-javaee8-security-api-2/
-/*
- * The interface HttpAuthenticationMechanism defines the following three methods:
-		validateRequest() ->	authenticates an HTTP request.
-		secureResponse()  ->	secures the HTTP response message.
-		cleanSubject() 	  ->	clears the subject of provided principals and credentials.
-		All methods accept the same parameter types, 
-		which are: HttpServletRequest, HttpServletResponse, and HttpMessageContext. 
-		These map to the corresponding methods defined on the JASPIC Server Auth Module interface, 
-		which is provided by the container. 
-		When a JASPIC method is invoked on Server Auth, 
-		it delegates to the corresponding method of your custom HttpAuthenticationMechanism.
- * */
-
-public class ContentAuthenticationMechanism implements HttpAuthenticationMechanism{
-	private Map<String,String> users;
-	private Map<String,Set<String>> roles;
-	
-	@Inject 
-	private IdentityStoreHandler identityStoreHandler;
-	
-	
-	@PostConstruct
-	public void init() {
-		users= new HashMap<>();roles= new HashMap<>();
-		System.out.println("@PostConstruct called on HAM implementation.");
-		users.put("admin", "admin");
-		users.put("user", "password");
-		users.put("guest", "guest");
-		
-		roles.put("admin", new HashSet<>(Arrays.asList("admin")));
-		roles.put("user", new HashSet<>(Arrays.asList("user")));
-		roles.put("guest", new HashSet<>(Arrays.asList("guest")));
-		
-	}
-	@Override
-	public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response,
-			HttpMessageContext httpMessageContext) throws AuthenticationException {
-		
-		String username= request.getParameter("username");
-		String password= request.getParameter("password");
-		UsernamePasswordCredential usernamePasswordCredential = new UsernamePasswordCredential(username, password);
-		System.out.println("username:"+username+" password:"+password);
-		if(username!=null && password != null) {
-			CredentialValidationResult credentialValidationResult = identityStoreHandler.validate(usernamePasswordCredential);
-			if(credentialValidationResult.getStatus() == Status.VALID) {
-				return httpMessageContext.notifyContainerAboutLogin(credentialValidationResult);
-			}
-			else {
-				return httpMessageContext.responseUnauthorized();
-			}
-		}
+//package com.sec.service;
+//
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.HashSet;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import javax.annotation.PostConstruct;
+//import javax.enterprise.context.ApplicationScoped;
+//import javax.inject.Inject;
+//import javax.security.enterprise.AuthenticationException;
+//import javax.security.enterprise.AuthenticationStatus;
+//import javax.security.enterprise.authentication.mechanism.http.AutoApplySession;
+//import javax.security.enterprise.authentication.mechanism.http.HttpAuthenticationMechanism;
+//
+//
+//import javax.security.enterprise.authentication.mechanism.http.HttpMessageContext;
+//import javax.security.enterprise.credential.UsernamePasswordCredential;
+//import javax.security.enterprise.identitystore.CredentialValidationResult;
+//import javax.security.enterprise.identitystore.CredentialValidationResult.Status;
+//import javax.security.enterprise.identitystore.IdentityStoreHandler;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//@ApplicationScoped
+////In order to ensure that it is available to the Java application, 
+////you will need to implement the HttpAuthenticationMechanism interface as a CDI bean with @ApplicationScope.
+//@AutoApplySession
+//
+////https://developer.ibm.com/tutorials/j-javaee8-security-api-2/
+///*
+// * The interface HttpAuthenticationMechanism defines the following three methods:
+//		validateRequest() ->	authenticates an HTTP request.
+//		secureResponse()  ->	secures the HTTP response message.
+//		cleanSubject() 	  ->	clears the subject of provided principals and credentials.
+//		All methods accept the same parameter types, 
+//		which are: HttpServletRequest, HttpServletResponse, and HttpMessageContext. 
+//		These map to the corresponding methods defined on the JASPIC Server Auth Module interface, 
+//		which is provided by the container. 
+//		When a JASPIC method is invoked on Server Auth, 
+//		it delegates to the corresponding method of your custom HttpAuthenticationMechanism.
+// * */
+//
+//public class ContentAuthenticationMechanism implements HttpAuthenticationMechanism{
+//	private Map<String,String> users;
+//	private Map<String,Set<String>> roles;
+//	
+//	@Inject 
+//	private IdentityStoreHandler identityStoreHandler;
+//	
+//	
+//	@PostConstruct
+//	public void init() {
+////		users= new HashMap<>();roles= new HashMap<>();
+//		System.out.println("@PostConstruct called on HAM implementation.");
+////		users.put("admin", "admin");
+////		users.put("user", "password");
+////		users.put("guest", "guest");
+////		
+////		roles.put("admin", new HashSet<>(Arrays.asList("admin")));
+////		roles.put("user", new HashSet<>(Arrays.asList("user")));
+////		roles.put("guest", new HashSet<>(Arrays.asList("guest")));
+//		
+//	}
+//	@Override
+//	public AuthenticationStatus validateRequest(HttpServletRequest request, HttpServletResponse response,
+//			HttpMessageContext httpMessageContext) throws AuthenticationException {
+//		
+//		String username= request.getParameter("username");
+//		String password= request.getParameter("password");
+//		UsernamePasswordCredential usernamePasswordCredential = new UsernamePasswordCredential(username, password);
+//		System.out.println("username:"+username+" password:"+password);
 //		if(username!=null && password != null) {
-//			if(users.containsKey(username)){
-//				if(users.get(username).equals(password)) {
-//					return httpMessageContext.notifyContainerAboutLogin(username,roles.get(username));
-//				}
+//			CredentialValidationResult credentialValidationResult = identityStoreHandler.validate(usernamePasswordCredential);
+//			if(credentialValidationResult.getStatus() == Status.VALID) {
+//				return httpMessageContext.notifyContainerAboutLogin(credentialValidationResult);
 //			}
-//			return httpMessageContext.responseUnauthorized();
-//			
-//					
+//			else {
+//				return httpMessageContext.responseUnauthorized();
+//			}
 //		}
-		return httpMessageContext.responseUnauthorized();
-	}
-	
-}
+////		if(username!=null && password != null) {
+////			if(users.containsKey(username)){
+////				if(users.get(username).equals(password)) {
+////					return httpMessageContext.notifyContainerAboutLogin(username,roles.get(username));
+////				}
+////			}
+////			return httpMessageContext.responseUnauthorized();
+////			
+////					
+////		}
+//		return httpMessageContext.responseUnauthorized();
+//	}
+//	
+//}
